4076e096cd5f92ea395da93137a27b581f8475b0
Harley Gotardo <harleygothards2@gmail.com>
2025-05-13 23:36:18 +0800

    Changed color

--- src/views/Authenticated/FPCRequest/View.vue
+++ src/views/Authenticated/FPCRequest/View.vue
@@ -486,7 +486,7 @@ const formatDateTime = (dateTimeString) => {
         <AlertDialogTrigger asChild>
           <Button
             title="Approve Request"
-            class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
+            class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-900 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
             @click="confirmApproveRequest"
           >
             <svg
